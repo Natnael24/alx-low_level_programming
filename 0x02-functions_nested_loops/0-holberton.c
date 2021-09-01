d769e5f5b924b4c92fa62376a8909bb3df1988a9
Natnael24 <natiman404@gmail.com>
2021-09-01 13:42:08 -0700

    start

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+const char* printChr(){
+
+return "_putchar";
+}
+
+int main(void){
+printf("%s", printChr());
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+c language functions and nested loops
