28acebf6cbc6efda5d49a8066a33717af118b7f2
Natnael24 <natiman404@gmail.com>
2021-09-01 08:39:00 -0700

    additional

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,13 +1,6 @@
 #include <stdio.h>
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
-	printf("Hello,world");
+	printf("Hello World\n");
     	return (0);
 }
-
