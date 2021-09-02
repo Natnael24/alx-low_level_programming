16c59655b50d5163642a632123cf7127169f9579
Natnael24 <88314084+Natnael24@users.noreply.github.com>
2021-09-02 17:31:38 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,14 +6,14 @@
  * Return: Always 0.
  */
 
-void print_alphabet_x10()
+void print_alphabet_x10(void)
 {
 const char s[] = "abcdefghijklmnopqrstuvwxyz";
 int i = 0;
 int j = 0;
-for (j = 0; j != 10; j++)
+    for (j = 0; j != 10; j++)
 {
- for (i = 0; s[i] != '\0'; i++)		
+    for (i = 0; s[i] != '\0'; i++)		
 {
 _putchar(s[i]);
 }
